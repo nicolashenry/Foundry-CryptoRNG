dfe8382ea1ad00f52f2f4654d7bd2210aadce3da
Nicolas HENRY <icewil@gmail.com>
2022-05-14 02:32:19 +0200

    refactor: :zap: pre-generate 1000 values

--- src/crypto-rng.ts
+++ src/crypto-rng.ts
@@ -1,5 +1,13 @@
 
-Hooks.once('init', () =>
-{
-	CONFIG.Dice.randomUniform = () => crypto.getRandomValues(new Uint32Array(1))[0] / 0x100000000;
+let i = 0;
+const randomValues = crypto.getRandomValues(new Uint32Array(1000));
+
+Hooks.once('init', () => {
+	CONFIG.Dice.randomUniform = () => {
+		if (i >= randomValues.length) {
+			crypto.getRandomValues(randomValues);
+			i = 0;
+		}
+		return randomValues[i++] / 0x100000000;
+	};
 });
